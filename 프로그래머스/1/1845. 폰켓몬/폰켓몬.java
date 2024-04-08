ef444980f8ee95ae58755ee2ac51cb2dc1d771d7
InhoO1 <133478038+InhoO1@users.noreply.github.com>
2024-04-08 22:24:19 +0900

    [level 1] Title: 폰켓몬, Time: 3.25 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        HashSet<Integer> hash = new HashSet<Integer>();
+        
+        for(int num : nums){
+            hash.add(num);
+        }
+        
+        if(hash.size() > nums.length / 2){
+            answer = nums.length / 2;
+        }else{
+            answer = hash.size();
+        }
+        
+        
+        return answer;
+    }
+}
